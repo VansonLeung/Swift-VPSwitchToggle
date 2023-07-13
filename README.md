adaa311646861e421c0a0999ef7189c195eca750
Vanson Wing Leung <vanson@vanportdev.com>
2023-07-13 11:57:45 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Swift-VPSwitchToggle
+
+A description of this package.
---
+++ Sources/Swift-VPSwitchToggle/Swift_VPSwitchToggle.swift
@@ -0,0 +1,6 @@
+public struct Swift_VPSwitchToggle {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
